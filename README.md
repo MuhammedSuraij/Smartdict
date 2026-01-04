c5081c5aa9d7777e2c5f82e27e5bc8074b93f989
MuhammedSuraij <msuraijcv@gmail.com>
2026-01-04 15:23:20 +0530

    2

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 *Day-1:
     Made the file structures and make a sample module for Python in C
+
+*Day-2:
+    Made a sample dict object and check whether it can use in Python
--- smart_dict.c
+++ smart_dict.c
@@ -1,24 +1,96 @@
 #include <Python.h>
 
-static PyObject* say_hello(PyObject* self,PyObject* args)
+typedef struct {
+    PyObject_HEAD
+    PyObject *dict;
+}SmartDictObject;
+
+static PyObject* SmartDict_new(PyTypeObject *type,PyObject *args,PyObject *kwds)
 {
-    return PyUnicode_FromString("Hello from smart dict");
+    SmartDictObject *self;
+    self = (SmartDictObject *)type->tp_alloc(type,0);
+    if (self !=NULL)
+    {
+        self->dict=PyDict_New();
+        if(!self->dict)
+        {
+            Py_DECREF(self);
+            return NULL;
+        }
+    }
+    return (PyObject *)self;
 }
 
-static PyMethodDef SmartDictMethods[]={
-    {"say_hello",say_hello,METH_NOARGS,"Say hello from c"},
-    {NULL,NULL,0,NULL}
+static void SmartDict_dealloc(SmartDictObject *self)
+{
+    Py_XDECREF(self->dict);
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static int SmartDict_setitem(PyObject *self,
+                             PyObject *key,
+                             PyObject *value)
+{
+    SmartDictObject *sd=(SmartDictObject *)self;
+    if(value== NULL)
+    {
+        PyErr_SetString(PyExc_TypeError,"Cannot delete items");
+        return -1;
+    }
+    return PyDict_SetItem(sd->dict,key,value);
+}
+
+static PyObject* SmartDict_getitem(PyObject *self,PyObject *key)
+{
+    SmartDictObject *sd=(SmartDictObject *)self;
+    PyObject *value=PyDict_GetItem(sd->dict,key);
+    if (!value)
+    {
+        PyErr_SetString(PyExc_KeyError,"Key Not Found");
+        return NULL;
+    }
+    Py_INCREF(value);
+    return value;
+    
+}
+
+static PyMappingMethods SmartDict_as_mapping ={
+    .mp_length=0,
+    .mp_subscript=SmartDict_getitem,
+    .mp_ass_subscript=SmartDict_setitem
 };
 
-static struct PyModuleDef smartdictmodule={
+static PyTypeObject SmartDictType ={
+    PyVarObject_HEAD_INIT(NULL,0)
+    .tp_name="smart_dict.SmartDict",
+    .tp_basicsize=sizeof(SmartDictObject),
+    .tp_itemsize =0,
+    .tp_dealloc=(destructor)SmartDict_dealloc,
+    .tp_flags=Py_TPFLAGS_DEFAULT,
+    .tp_doc="SmartDict object",
+    .tp_as_mapping=&SmartDict_as_mapping,
+    .tp_new=SmartDict_new,
+};
+
+static PyModuleDef smartdictmodule=
+{
     PyModuleDef_HEAD_INIT,
     "smart_dict",
-    "SmartDict C Extension",
+    "Smart Dictionary Module",
     -1,
-    SmartDictMethods
 };
 
 PyMODINIT_FUNC PyInit_smart_dict(void)
 {
-    return PyModule_Create(&smartdictmodule);
+    PyObject *m;
+    if(PyType_Ready(&SmartDictType)<0)
+        return NULL;
+    
+    m=PyModule_Create(&smartdictmodule);
+    if(!m)
+        return NULL;
+    
+    Py_INCREF(&SmartDictType);
+    PyModule_AddObject(m,"SmartDict",(PyObject *)&SmartDictType);
+    return m;
 }
